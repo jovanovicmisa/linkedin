9c32f44ed7ed4bf213b7ca76fc79d98bcf75ffa8
Jovanovic Misa <misa.jovanovic84@gmail.com>
2016-05-05 12:34:27 +0200

    Update script.js

--- authentication/js/script.js
+++ authentication/js/script.js
@@ -1,9 +1,9 @@
-function getParameter (){
-   var url = document.URL;
-		var queryParam = url.substring( url.indexOf('=') + 1 , url.lastIndexOf('&'));
- 		document.write(queryParam);
- 		return queryParam;
-}
+// function getParameter (){
+//    var url = document.URL;
+// 		var queryParam = url.substring( url.indexOf('=') + 1 , url.lastIndexOf('&'));
+//  		document.write(queryParam);
+//  		return queryParam;
+// }
 function getParameterByName(name){
 	var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
 	document.write(match[1]);
