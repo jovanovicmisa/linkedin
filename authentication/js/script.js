5b4f890fca313aa1e95e9db865ce6fe7525026cf
Misa Jovanovic <misa.jovanovic@consulteer.com>
2016-05-05 09:49:57 +0200

    add script method

--- authentication/js/script.js
+++ authentication/js/script.js
@@ -1,5 +1,10 @@
-function getParameterByName() {
+function getParameter (){
    var url = document.URL;
-		var queryString = url.substring( url.indexOf('?') + 1 , url.lastIndexOf('&'));
+		var queryString = url.substring( url.indexOf('=') + 1 , url.lastIndexOf('&'));
  		document.write(queryString);
+}
+function getParameterByName(name){
+	var match = RegExp(name + '=([^&]*)').exec(window.location);
+	document.write(match[1]);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
 }
